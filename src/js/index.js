c0b404a66f032b7dc0c7e325452048133ff74fff
lolauriss <lolauriss@gmail.com>
2019-08-21 21:38:14 +0000

    DomainNameGenerator

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,24 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const nameGenerator = () => {
+    let nameDomain = [];
+
+    for (let i in pronoun) {
+      for (let j in adj) {
+        for (let k in noun) {
+          nameDomain.push;
+        }
+      }
+    }
+
+  let myList = "";
+  for (let i = 0; i < nameDomain.length; i++) {
+    myList += "<li>${nameDomain[i]}.com</li>";
+  }
+  document.querySelector("#generator").innerHTML = "<ul>${myList}</ul>";
 };
